c11bd1455475075cc94da820bcafb1c7db7547d4
Alfinanasdeen <alfinanasdeen@gmail.com>
2024-05-28 00:25:35 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import dotenv from "dotenv";
-dotenv.config();
+import dotenv from 'dotenv';
+dotenv.config(); 
 
 import express from "express";
 import cors from "cors";
@@ -14,15 +14,12 @@ const PORT = process.env.PORT || 3000;
 
 connectToMongoDB();
 
-//app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
-// Allow requests from the origin of your frontend application
-app.use(cors({ origin: "https://shimmering-kheer-8dc5c2.netlify.app" }));
+
+app.use(cors({origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", userRoute);
 app.use("/api", urlShortenerRoute);
 
-app.listen(PORT, () =>
-  console.log(`Server running at http://${hostname}:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running at http://${hostname}:${PORT}`));

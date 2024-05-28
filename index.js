6f1cabf39e12553752fd29175e4621a382eb4efe
Alfinanasdeen <alfinanasdeen@gmail.com>
2024-05-28 10:54:33 +0530

    CORS configuration

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config(); 
+import dotenv from "dotenv";
+dotenv.config();
 
 import express from "express";
 import cors from "cors";
@@ -14,12 +14,26 @@ const PORT = process.env.PORT || 3000;
 
 connectToMongoDB();
 
-
-app.use(cors({origin: "*", credentials: true }));
+// CORS configuration
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      console.log("Origin:", origin);
+      if (origin === process.env.FRONTEND_URL || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", userRoute);
 app.use("/api", urlShortenerRoute);
 
-app.listen(PORT, () => console.log(`Server running at http://${hostname}:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running at http://${hostname}:${PORT}`)
+);
